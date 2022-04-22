df3099a6b52581825ec9b1ce83ceedc47b2eb73e
BrettMM88 <BrettMM88@gmail.com>
2022-04-21 19:14:22 -0500

    Updated footer with credit for animation

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,6 +5,9 @@ const Footer = () => {
     return (
         <div class="footer-container .container-fluid">
             <span class="footer-text">&copy; Brett Magee 2022</span>
+            <span class="footer-text">
+                Credit to <a href="https://codepen.io/WebSonick" target="_blank" rel="noopener noreferrer">@WebSonick</a> for the Cloud Animation
+            </span>
         </div>
     )
 }
